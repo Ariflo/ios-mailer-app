e17bcb2182cb8c48eb96f28dbd6bbc548eb45203
Arian Flores <arian@arianflores.com>
2020-12-01 12:57:46 -0800

    Sets up Initial Login Screen (#2)

--- Addressable/Addressable/ContentView.swift
+++ Addressable/Addressable/ContentView.swift
@@ -5,22 +5,69 @@
 //  Created by Arian Flores on 12/1/20.
 //
 
+import UIKit
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
+    @State var appleSignInDelegates: SignInWithAppleDelegates! = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("AddressableAssests")
+            
+            UserAndPassword()
+                .padding()
+            
+            SignInAddressableWithApple()
+                .frame(width: 280, height: 60)
+                .onTapGesture(perform: showAppleLogin)
+            }.onAppear() {
+                self.performExistingAccountSetupFlows()
+            }
+    }
+    
+    private func showAppleLogin() {
+      let request = ASAuthorizationAppleIDProvider().createRequest()
+
+      request.requestedScopes = [.fullName, .email]
+      performSignIn(using: [request])
+    }
+    
+    private func performSignIn(using requests: [ASAuthorizationRequest]) {
+      appleSignInDelegates = SignInWithAppleDelegates() { success in
+        if success {
+          // update UI
+        } else {
+          // show the user an error
+        }
+      }
+
+      let controller = ASAuthorizationController(authorizationRequests: requests)
+      controller.delegate = appleSignInDelegates
+
+      controller.performRequests()
+    }
+    
+    private func performExistingAccountSetupFlows() {
+      #if !targetEnvironment(simulator)
+
+      let requests = [
+        ASAuthorizationAppleIDProvider().createRequest(),
+        ASAuthorizationPasswordProvider().createRequest()
+      ]
+
+      performSignIn(using: requests)
+      #endif
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-            ContentView()
-            ContentView()
-            ContentView()
         }
     }
 }
+#endif
---
+++ Addressable/Addressable/UserAndPassword.swift
@@ -0,0 +1,73 @@
+//
+//  UserAndPassword.swift
+//  Addressable
+//
+//  Created by Arian Flores on 12/1/20.
+//
+
+import SwiftUI
+
+struct UserAndPassword: View {
+  @State var username: String = ""
+  @State var password: String = ""
+  @State var showingAlert = false
+  @State var alertText: String = ""
+
+  var body: some View {
+    VStack {
+      TextField("Username", text: $username)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .textContentType(.username)
+        .autocapitalization(.none)
+        .disableAutocorrection(true)
+
+      SecureField("Password", text: $password)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .textContentType(.password)
+
+      Button(action: signInTapped) {
+        Text("Log In")
+          .foregroundColor(Color.gray)
+      }
+      .alert(isPresented: $showingAlert) {
+        Alert(title: Text(alertText))
+      }
+    }
+    .padding()
+  }
+
+  private func signInTapped() {
+    let ws = CharacterSet.whitespacesAndNewlines
+
+    let account = username.trimmingCharacters(in: ws)
+    let pwd = password.trimmingCharacters(in: ws)
+
+    guard !(account.isEmpty || pwd.isEmpty) else {
+      alertText = "Please enter a username and password."
+      showingAlert = true
+      return
+    }
+
+    /**
+      * Putting the user/pwd into the shared web credentials ensures that
+      * it's available for your browser based (Safari) logins if you haven't implemented
+      * the web version of Sign in with Apple but also then makes it available
+      * for future logins via Sign in with Apple on  iOS devices.
+     
+         SharedWebCredential(domain: "")
+           .store(account: account, password: password) { result in
+             guard case .failure = result else { return }
+             self.alertText = "Failed to store password."
+             self.showingAlert = true
+         }
+     */
+  }
+}
+
+#if DEBUG
+struct UserAndPassword_Previews: PreviewProvider {
+  static var previews: some View {
+    UserAndPassword()
+  }
+}
+#endif
