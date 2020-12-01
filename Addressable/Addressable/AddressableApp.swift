4886e53097fa3f7c872f654492ab7e56abdd13f6
Arian Flores <arian@arianflores.com>
2020-12-01 10:05:59 -0800

    Applies initial application setup (#1)

---
+++ Addressable/Addressable/AddressableApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressableApp.swift
+//  Addressable
+//
+//  Created by Arian Flores on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct AddressableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Addressable/Addressable/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Addressable
+//
+//  Created by Arian Flores on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+            ContentView()
+            ContentView()
+        }
+    }
+}
