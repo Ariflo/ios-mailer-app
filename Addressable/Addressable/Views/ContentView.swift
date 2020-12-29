35653b36da63d92e4ea7a48a5c40f1b13265f35e
Arian Flores <arian@arianflores.com>
2020-12-29 11:30:20 -0800

    Adds linter and reorgs files (#4)

--- Addressable/Addressable/Views/ContentView.swift
+++ Addressable/Addressable/Views/ContentView.swift
@@ -11,54 +11,54 @@ import AuthenticationServices
 
 struct ContentView: View {
     @State var appleSignInDelegates: SignInWithAppleDelegates! = nil
-    
+
     var body: some View {
         VStack {
             Image("AddressableAssests")
-            
+
             UserAndPassword()
                 .padding()
-            
+
             SignInAddressableWithApple()
                 .frame(width: 280, height: 60)
                 .onTapGesture(perform: showAppleLogin)
-            }.onAppear() {
-                self.performExistingAccountSetupFlows()
-            }
-    }
-    
-    private func showAppleLogin() {
-      let request = ASAuthorizationAppleIDProvider().createRequest()
-
-      request.requestedScopes = [.fullName, .email]
-      performSignIn(using: [request])
-    }
-    
-    private func performSignIn(using requests: [ASAuthorizationRequest]) {
-      appleSignInDelegates = SignInWithAppleDelegates() { success in
-        if success {
-          // update UI
-        } else {
-          // show the user an error
+        }.onAppear {
+            self.performExistingAccountSetupFlows()
         }
-      }
-
-      let controller = ASAuthorizationController(authorizationRequests: requests)
-      controller.delegate = appleSignInDelegates
-
-      controller.performRequests()
     }
-    
+
+    private func showAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+
+        request.requestedScopes = [.fullName, .email]
+        performSignIn(using: [request])
+    }
+
+    private func performSignIn(using requests: [ASAuthorizationRequest]) {
+        appleSignInDelegates = SignInWithAppleDelegates { success in
+            if success {
+                // update UI
+            } else {
+                // show the user an error
+            }
+        }
+
+        let controller = ASAuthorizationController(authorizationRequests: requests)
+        controller.delegate = appleSignInDelegates
+
+        controller.performRequests()
+    }
+
     private func performExistingAccountSetupFlows() {
-      #if !targetEnvironment(simulator)
+        #if !targetEnvironment(simulator)
 
-      let requests = [
-        ASAuthorizationAppleIDProvider().createRequest(),
-        ASAuthorizationPasswordProvider().createRequest()
-      ]
+        let requests = [
+            ASAuthorizationAppleIDProvider().createRequest(),
+            ASAuthorizationPasswordProvider().createRequest()
+        ]
 
-      performSignIn(using: requests)
-      #endif
+        performSignIn(using: requests)
+        #endif
     }
 }
 
--- Addressable/Addressable/Views/MailingsView.swift
+++ Addressable/Addressable/Views/MailingsView.swift
@@ -9,30 +9,30 @@ import SwiftUI
 
 struct MailingsView: View {
     @State private var mailingItems: [MailingMailing] = []
-    
+
     var body: some View {
         VStack {
             Text("Addressable Mailings").font(.title)
             Spacer()
-            
+
             List(mailingItems) { mailing in
                 Text(mailing.name)
             }
             Spacer()
         }.navigationBarHidden(true).onAppear { getMailings() }
     }
-    
+
     func getMailings() {
-        guard let url =  URL(string:"http://localhost:3000/api/v1/mailings") else {
+        guard let url = URL(string: "http://localhost:3000/api/v1/mailings") else {
             return
         }
-        
+
         var request = URLRequest(url: url)
-        
+
         request.setValue("application/json", forHTTPHeaderField: "ContentType")
         request.setValue("ari+123@addressable.app", forHTTPHeaderField: "X-User-Email")
         request.setValue("9BhZZxj87iZCriJtbUVgkz2n", forHTTPHeaderField: "X-User-Token")
-        
+
 
         URLSession.shared.dataTask(with: request) { data, response, error in
             if error != nil {
@@ -41,23 +41,23 @@ struct MailingsView: View {
             }
             // Verify data is not nil
             guard let data = data else {
-                  print("NO DATA")
-                  return
+                print("NO DATA")
+                return
             }
-            
+
             // Verify results as String
             guard let result = String(data: data, encoding: .utf8)  else {
-                    return
+                return
             }
             print("THIS IS THE RESULT ->", result)
-            
+
             // Add JSON Decoding in URLSession closure
             let decoder = JSONDecoder()
             guard let response = try? decoder.decode(MailingsResponse.self, from: data) else {
                 print("NO RESPONSE DATA")
-              return
+                return
             }
-            
+
             print("THIS IS THE RESPONSE ->", response)
             // Update mailing items on main thread
             DispatchQueue.main.async {
@@ -65,7 +65,7 @@ struct MailingsView: View {
                     mailings.append(mailingElement.mailing)
                 }
             }
-          }.resume()
+        }.resume()
     }
 }
 
